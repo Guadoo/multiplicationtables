3040c905b307f6e82f0b73a8c8708562a7447160
Liu Jingkai <rock_jk@msn.com>
2021-05-19 17:37:34 +0800

    Initial Commit

---
+++ multiplicationtables/multiplicationtablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiplicationtablesApp.swift
+//  multiplicationtables
+//
+//  Created by Guadoo on 2021/5/19.
+//
+
+import SwiftUI
+
+@main
+struct multiplicationtablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
