d40502a687e4c34972a29b545923037174d21901
Liu Jingkai <rock_jk@msn.com>
2021-05-21 11:40:53 +0800

    Separate setting section and game section and display them during setting mode or game mode

--- multiplicationtables/ContentView.swift
+++ multiplicationtables/ContentView.swift
@@ -21,107 +21,128 @@ struct ContentView: View {
     @State private var inputResult = ""
     @State private var adjustment = ""
     
+    @State private var correctAnswer = 0
+    
+    @State private var hiddenSetting = false
+    
     var body: some View {
        
         VStack {
-            Section {
-                Stepper(value: $matrixValue, in: range, step: step) {
-                    Text("Multiplication Table is \(matrixValue) Up to 12 ")
-                }
-                .padding(10)
+            
+            if !hiddenSetting {
+                Section {
+                    Stepper(value: $matrixValue, in: range, step: step) {
+                        Text("Multiplication Table is \(matrixValue) Up to 12 ")
+                    }
+                    .padding(10)
 
-                Picker(selection: $questionNumber, label: Text("Please select question number")) {
-                    Text("5").tag(5)
-                    Text("10").tag(10)
-                    Text("20").tag(20)
-                    Text("All").tag(0)
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                
-                if tables.isEmpty {
-                    Button(action: {
-                        tables = multiplicationTable(matrixValue: matrixValue, questionNumber: questionNumber)
-                        inputResult.removeAll()
-                    }, label: {
-                        Text("Start")
-                    })
-                        .padding()
-                } else {
-                    Text("Please complete the questions!")
-                        .padding()
-                }
-            }
-            
-            HStack{
-                if tables.isEmpty {
-                    Text("? X ? = ?")
-                } else {
-                    Text("\(tables.first!.0) X \(tables.first!.1) = ")
-                    Text(inputResult)
-                }
-            }
-            Text(adjustment)
-            
-            VStack {
-                HStack{
-                    Button("1") {
-                        inputResult += "1"
-                    }.padding()
-                    Button("2") {
-                        inputResult += "2"
-                    }.padding()
-                    Button("3") {
-                        inputResult += "3"
-                    }.padding()
-                }
-                HStack{
-                    Button("4") {
-                        inputResult += "4"
-                    }.padding()
-                    Button("5") {
-                        inputResult += "5"
-                    }.padding()
-                    Button("6") {
-                        inputResult += "6"
-                    }.padding()
-                }
-                HStack{
-                    Button("7") {
-                        inputResult += "7"
-                    }.padding()
-                    Button("8") {
-                        inputResult += "8"
-                    }.padding()
-                    Button("9") {
-                        inputResult += "9"
-                    }.padding()
-                }
-                HStack{
-                    Button("D") {
-                        if inputResult.count > 0 {
-                            inputResult.removeLast()
-                        }
-                    }.padding()
-                    Button("0") {
-                        inputResult += "0"
-                    }.padding()
-                    Button("=") {
-                        if tables.isEmpty {
+                    Picker(selection: $questionNumber, label: Text("Please select question number")) {
+                        Text("5").tag(5)
+                        Text("10").tag(10)
+                        Text("20").tag(20)
+                        Text("All").tag(0)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    if tables.isEmpty {
+                        Button(action: {
+                            tables = multiplicationTable(matrixValue: matrixValue, questionNumber: questionNumber)
                             inputResult.removeAll()
-                        } else {
-                            if Int(inputResult) == tables.first!.0 * tables.first!.1 {
-                                inputResult.removeAll()
-                                tables.removeFirst()
-                                adjustment = ""
-                            } else {
-                                inputResult.removeAll()
-                                adjustment = "X"
-                            }
-                        }
-                    }.padding()
+                            correctAnswer = 0
+                            hiddenSetting = true
+                        }, label: {
+                            Text("Start")
+                        })
+                            .padding()
+                    } else {
+                        Text("Please complete the questions!")
+                            .padding()
+                    }
                 }
-            
-                Text("Rest Questions : \(tables.count)")
+            } else {
+                Section {
+                    HStack{
+                        if tables.isEmpty {
+                            Text("? X ? = ?")
+                        } else {
+                            Text("\(tables.first!.0) X \(tables.first!.1) = ")
+                            Text(inputResult)
+                        }
+                    }
+                    
+                    Text(adjustment)
+                    
+                    VStack {
+                        HStack{
+                            Button("1") {
+                                inputResult += "1"
+                            }.padding()
+                            Button("2") {
+                                inputResult += "2"
+                            }.padding()
+                            Button("3") {
+                                inputResult += "3"
+                            }.padding()
+                        }
+                        HStack{
+                            Button("4") {
+                                inputResult += "4"
+                            }.padding()
+                            Button("5") {
+                                inputResult += "5"
+                            }.padding()
+                            Button("6") {
+                                inputResult += "6"
+                            }.padding()
+                        }
+                        HStack{
+                            Button("7") {
+                                inputResult += "7"
+                            }.padding()
+                            Button("8") {
+                                inputResult += "8"
+                            }.padding()
+                            Button("9") {
+                                inputResult += "9"
+                            }.padding()
+                        }
+                        HStack{
+                            Button("D") {
+                                if inputResult.count > 0 {
+                                    inputResult.removeLast()
+                                }
+                            }.padding()
+                            Button("0") {
+                                inputResult += "0"
+                            }.padding()
+                            Button("=") {
+                                if tables.isEmpty {
+                                    inputResult.removeAll()
+                                    correctAnswer = 0
+                                } else {
+                                    if Int(inputResult) == tables.first!.0 * tables.first!.1 {
+                                        inputResult.removeAll()
+                                        tables.removeFirst()
+                                        adjustment = ""
+                                        correctAnswer += 1
+                                    } else {
+                                        inputResult.removeAll()
+                                        adjustment = "X"
+                                    }
+                                }
+                            }.padding()
+                        }
+                        Text("Correct : \(correctAnswer)").padding(2)
+                        Text("Remaining Questions : \(tables.count)")
+                        
+                        if tables.isEmpty {
+                            Button("Go Back") {
+                                hiddenSetting = false
+                            }
+                            .padding()
+                        }
+                    }
+                }
             }
         }
     }
